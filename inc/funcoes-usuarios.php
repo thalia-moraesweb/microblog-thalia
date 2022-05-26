35cc93afdd971f1be0ec95fc544e64cf6b575af5
thalia-moraesweb <moraesthalia.silva@gmail.com>
2022-05-26 16:57:58 -0300

    update
    acrescentação do update para mostrar os campos

--- inc/funcoes-usuarios.php
+++ inc/funcoes-usuarios.php
@@ -48,7 +48,13 @@ function lerUsuarios(mysqli $conexao):array {
 
 
 // Função lerUmUsuario: usada em usuario-atualiza.php
+function lerUmUsuario(mysqli $conexao, int $id):array {
+    $sql = "SELECT id, nome, email, tipo, senha FROM usuarios WHERE id = $id";
 
+    $resultado = mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+
+    return mysqli_fetch_assoc($resultado);
+}
 // fim lerUmUsuario
 
 
@@ -60,7 +66,11 @@ function lerUsuarios(mysqli $conexao):array {
 
 
 // Função atualizarUsuario: usada em usuario-atualiza.php
-
+function atualizarUsuario(mysqli $conexao, int $id, string $nome, string $email, string $senha, string $tipo){
+    $sql = "UPDATE usuarios SET nome = '$nome', email = '$email', senha = '$senha', tipo = '$tipo' WHERE id = $id";
+    
+    mysqli_query($conexao, $sql) or die(mysqli_error($conexao));
+}
 // fim atualizarUsuario
 
 
